28f2c8f4d9e16eef3362c92ef254132f1bda546a
Bernhard Windisch <bernhard.windisch@doob.at>
2020-11-15 14:34:32 +0100

    Add Properties to ScriptContext

--- source/Actions/middler.Action.Scripting/Models/ScriptContextRequest.cs
+++ source/Actions/middler.Action.Scripting/Models/ScriptContextRequest.cs
@@ -12,6 +12,13 @@ namespace middler.Action.Scripting.Models
         public MiddlerRouteData RouteData => _middlerRequestContext.RouteData;
         public SimpleDictionary<string> Headers => _middlerRequestContext.Headers;
         public MiddlerRouteQueryParameters QueryParameters => _middlerRequestContext.QueryParameters;
+        public string User => Authenticated ? _middlerRequestContext.Principal.Identity?.Name : "Anonymous";
+
+        public string Client =>
+            _middlerRequestContext.Principal.Claims.FirstOrDefault(c => c.Type == "client_id")?.Value;
+
+        public bool Authenticated => _middlerRequestContext.Principal.Identity?.IsAuthenticated ?? false;
+
         public string ClientIp=> _middlerRequestContext.SourceIPAddress.ToString();
         public string[] ProxyServers => _middlerRequestContext.ProxyServers.Select(ip => ip.ToString()).ToArray();
 
